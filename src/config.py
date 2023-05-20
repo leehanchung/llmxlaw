66aed8095493b70256dcb50f816e21c7a32f8716
PlainSite <help@plainsite.org>
2023-05-20 15:04:02 -0700

    Update config.py

--- src/config.py
+++ src/config.py
@@ -5,8 +5,8 @@ CONFIG = dict(
     legal_intaker_name = "Michael Ross",
     company_name="Pearson Hardman",
     conversation_stage_dict=conversation_stages,
-    conversation_purpose = "find out whether they are looking to achieve better sleep via buying a premier mattress.",
-    conversation_history=['Hello, this is Ted Lasso from Sleep Haven. How are you doing today? <END_OF_TURN>','User: I am well, howe are you?<END_OF_TURN>'],
+    conversation_purpose = "help generate a legal complaint that would be ready to file in civil court.",
+    conversation_history=['Hello, this is the Stanford CodeX Legal Complaint Generator. Please tell me your name? <END_OF_TURN>'],
     conversation_type="call",
-    conversation_stage = conversation_stages.get('1', "Introduction: Start the conversation by introducing yourself and your company. Be polite and respectful while keeping the tone of the conversation professional.")
+    conversation_stage = conversation_stages.get('1', "Introduction: Start the conversation by introducing yourself explaining that you are here to help generate a formal complaint. Be polite and respectful while keeping the tone of the conversation professional. Your greeting should be welcoming.")
 )
